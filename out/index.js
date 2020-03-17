166e0dbe15f043568137a970df5dc2e3a1e14a8e
Forrest <rainforest92@126.com>
2020-03-18 00:30:09 +0800

    :rocket: Rebuild

--- out/index.js
+++ out/index.js
@@ -1,2 +1,6 @@
-export * from './src/index';
+export { default as builtInValidators } from './builtIn/validators';
+export { default as localizeRules } from './builtIn/rules';
+import VusionValidator from './VusionValidator';
+export { VusionValidator };
+export default VusionValidator;
 //# sourceMappingURL=index.js.map
--- out/src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as builtInValidators } from './builtIn/validators';
-export { default as localizeRules } from './builtIn/rules';
-import VusionValidator from './VusionValidator';
-export { VusionValidator };
-export default VusionValidator;
-//# sourceMappingURL=index.js.map
